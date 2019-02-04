929e214cf90b8b3369dc3c9165067c378ca426a1
Donald Atkinson <fuzzicallogic@outlook.com>
2019-02-03 18:47:44 -0700

    Test Automation

---
+++ test/init.js
@@ -0,0 +1,24 @@
+import { isMainThread } from "worker_threads";
+
+describe('Unit Tests', () => {
+
+    before((client, done) => {
+        done();
+    });
+
+    beforeEach((client, done) => {
+        done();
+    });
+
+    afterEach((client, done) => {
+        done();
+    });
+
+    after((client, done) => {
+        done();
+    });
+
+    it('is a dummy test', (client) => {
+        client.assert.ok(true);
+    });
+});
